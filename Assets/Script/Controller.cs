8ff0d0264a4fb8f994662c2197a14493330e8dff
Kreangkri <kreangkri.klam@bumail.net>
2024-03-21 16:43:22 +0700

    Re model

--- Assets/Script/Controller.cs
+++ Assets/Script/Controller.cs
@@ -1,75 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
+public class MovementController : MonoBehaviour
 {
-    public Animator animator;
-    public float walkSpeed = 5f;
-    public float runSpeed = 10f;
-    public float jumpForce = 10f;
-    public float rotationSpeed = 1f;
-    private Vector3 moveDirection;
-    private bool isGrounded;
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 120f; 
     private Rigidbody rb;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //Input Move X and Y
+        // Input
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        //Calculate direction character move
-        Vector3 cameraForward = Camera.main.transform.forward;
-        Vector3 cameraRight = Camera.main.transform.right;
-        cameraForward.y = 0f;
-        cameraRight.y = 0f;
-        cameraForward.Normalize();
-        cameraRight.Normalize();
-        moveDirection = cameraForward * moveVertical + cameraRight * moveHorizontal;
+        // Direction move
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical).normalized * moveSpeed * Time.deltaTime;
 
-        //Rotate Character follow
-        if (moveDirection != Vector3.zero)
-        {
-            Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-        }
+        // Move the Rigidbody
+        rb.MovePosition(transform.position + transform.TransformDirection(movement));
 
-        //Input Jump
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        // Rotate the player
+        if (movement != Vector3.zero)
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isGrounded = false;
-        }
+            // Calculate the rotation angle
+            float targetAngle = Mathf.Atan2(moveHorizontal, moveVertical) * Mathf.Rad2Deg;
 
-        void FixedUpdate()
-        {
-            //Direction character move
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                rb.MovePosition(rb.position + moveDirection.normalized * runSpeed * Time.fixedDeltaTime);
-            }
-            else
-            {
-                rb.MovePosition(rb.position + moveDirection.normalized * walkSpeed * Time.fixedDeltaTime);
-            }
-        }
-        void OnCollisionEnter(Collision collision)
-        {
-            //Check character to ground
-            if (collision.gameObject.CompareTag("Ground"))
-            {
-                isGrounded = true;
-            }
-        }
+            // Create the rotation vector
+            Vector3 rotationVector = new Vector3(0f, targetAngle, 0f);
 
+            // Rotate the player smoothly
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(rotationVector), rotationSpeed * Time.deltaTime);
+        }
     }
 }
