5bdce2499f098ffb33ec5a4a321146d467f4a3fd
Chaiyanon <chaiyanon.prom@bumail.net>
2024-03-22 19:42:03 +0700

    Create UI PauseMenu And Script PauseManu

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool PauseGame = false;
+    public GameObject pauseMenuUI;
+    public Behaviour PauseCanvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseCanvas.enabled = !PauseCanvas.enabled;
+
+            if (!PauseCanvas.enabled)
+            {
+                PauseGame = true;
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() 
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        PauseGame =  false;
+        PauseCanvas.enabled = PauseCanvas.enabled;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void PauseHowToPlay()
+    {
+        Debug.Log("Loading HowToPlay...");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+}
