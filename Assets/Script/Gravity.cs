f0babde0d342170995da3fc021b104e44e8aedbf
Kreangkri <kreangkri.klam@bumail.net>
2024-03-23 08:52:39 +0700

    Code done : Next is you Armm!!
    -Done camera controller
    -Done Gravity Mechanic Controller
    -Done Player Controller
    -Done Animation Controller

---
+++ Assets/Script/Gravity.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class ObjectManipulation : MonoBehaviour
+{
+    [SerializeField] private float attractionForce = 10f; //Attraction Force
+    [SerializeField] private float repulsionForce = 10f; //Repulsion Force
+    [SerializeField] private float interactionRadius = 10f; //Radius
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            AttractObjects();
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            RepelObjects();
+        }
+    }
+    //Find objects
+    private void AttractObjects()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, interactionRadius);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("Gravity"))
+            {
+                Vector3 direction = (transform.position - collider.transform.position).normalized;
+                collider.GetComponent<Rigidbody>().AddForce(direction * attractionForce);
+            }
+        }
+    }
+
+    private void RepelObjects()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, interactionRadius); 
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("Gravity"))
+            {
+                Vector3 direction = (collider.transform.position - transform.position).normalized;
+                collider.GetComponent<Rigidbody>().AddForce(direction * repulsionForce);
+            }
+        }
+    }
+
+    //Display radius
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, interactionRadius); 
+    }
+}
